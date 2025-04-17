2ef11ac30958942879907a238b0c8d52ae927638
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2025-04-17 18:32:37 +0200

    script to update benchmark definitions with verdicts from run results

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# chc-comp25-scripts
-Scripts used in CHC-COMP 2025
+# Scripts used in CHC-COMP 2025
+
+## Environment
+
+- Python3 with packages `z3-solver`, `pyyaml`
+- [Benchexec](https://github.com/sosy-lab/benchexec)
+
+## Smoke tests
+
+1. Get benchmarks from CHC-COMP 2024
+
+    git clone https://github.com/chc-comp/chc-comp24-benchmarks.git ../chc-comp24-benchmarks
+
+2. Create some `*.yml` files for [benchexec](https://github.com/sosy-lab/benchexec)
+
+    make yml # SOLVER="z3 -t:500
+    make yml SOLVER="echo unknown"
+
---
+++ update-benchexec-yml.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import yaml
+import re
+
+prefix = sys.argv[1]
+test_results = sys.argv[2]
+
+with open(test_results, "r") as results:
+
+    for line in results.readlines():
+        if line.startswith(prefix):
+            entries = re.split(r'\s+', line.strip())
+            description_yml = entries[0]
+            new_verdict = entries[1]
+
+            data = None
+            update = False
+
+            with open(description_yml, "r") as file:
+                data = yaml.safe_load(file)
+                for property in data['properties']:
+                    if property["property_file"] == "../properties/check-sat.prp":
+                        if "expected_verdict" in property:
+                            old_verdict = property["expected_verdict"]
+                        else:
+                            old_verdict = None
+                        
+                        if new_verdict == "true" or new_verdict == "false":
+                            new_verdict = bool(new_verdict)
+
+                            if old_verdict:
+                                assert old_verdict == new_verdict, "mismatching verdict" + str(old_verdict) + " ~> " + str(new_verdict)
+                                print("keeping verdict for  ", description_yml)
+                            else:
+                                property["expected_verdict"] = new_verdict
+                                print("updating verdict for ", description_yml)
+                                update = True
+
+
+            if data and update:
+                with open(description_yml, "w") as file:
+                    yaml.dump(data, file)
