ace394fcb7354742256ec5bb28be26e74e12335d
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2025-05-03 21:59:35 -0400

    full run xmls

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Scripts used in CHC-COMP 2025
 
+**Warning: This repository is work in progress.**
+
 ## Environment
 
 - Python3 with packages `z3-solver`, `pyyaml`
@@ -16,3 +18,20 @@
     make yml # SOLVER="z3 -t:500
     make yml SOLVER="echo unknown"
 
+3. Run some tools
+
+    # ./smoke.sh chc2c-svcomp chc2c
+    ./smoke.sh ChocoCatalia chococatalia
+    ./smoke.sh eldarica-2.2 eldarica
+    ./smoke.sh golem-x64-linux golem
+    ./smoke.sh LoAT loat
+    ./smoke.sh coar mucyc
+    ./smoke.sh coar pcsat
+    ./smoke.sh ThetaCHC theta
+    ./smoke.sh UltimateTreeAutomizer ultimatetreeautomizer
+    ./smoke.sh UltimateUnihorn ultimateunihorn
+
+## Preparing the 2025 benchmarks
+
+- new script `classify.py`
+- `format.py` only translates about 1/3 of the benchmarks, data type names are not escaped if needed
--- update-benchexec-yml.py
+++ update-benchexec-yml.py
@@ -18,10 +18,15 @@ def truth(verdict):
             return None
 
 with open(test_results, "r") as results:
+
     for line in results.readlines():
-        if line.startswith(prefix):
+        if ".yml" in line:
             entries = re.split(r'\s+', line.strip())
-            description_yml = entries[0]
+
+            if len(entries) < 2:
+                continue
+
+            description_yml = prefix + "/" + entries[0]
             new_verdict = entries[1]
 
             data = None
@@ -39,14 +44,15 @@ with open(test_results, "r") as results:
                         if new_verdict == "true" or new_verdict == "false":
                             new_verdict = truth(new_verdict)
 
-                            if old_verdict:
-                                assert old_verdict == new_verdict, "mismatching verdict" + old_verdict + " ~> " + new_verdict
-                                print("keeping verdict for  ", description_yml)
+                            if old_verdict is not None:
+                                if old_verdict != new_verdict:
+                                    print("wrong verdict " + str(new_verdict) + " for " + description_yml)
+                                else:
+                                    print("keeping verdict for  " + description_yml)
                             else:
                                 property["expected_verdict"] = new_verdict
-                                print("updating verdict for ", description_yml)
+                                print("updating verdict for " + description_yml)
                                 update = True
-
 
             if data and update:
                 with open(description_yml, "w") as file:
