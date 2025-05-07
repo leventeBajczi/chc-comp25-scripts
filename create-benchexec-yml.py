a78847945603387c45236dbe411737c7f9058134
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2025-05-07 15:29:53 -0400

    make property file relative

--- create-benchexec-yml.py
+++ create-benchexec-yml.py
@@ -5,14 +5,30 @@ import os
 import yaml
 import subprocess
 
-benchmark_smt2 = sys.argv[1]
-description_yml = sys.argv[2]
+property_prp = sys.argv[1]
+benchmark_smt2 = sys.argv[2]
+description_yml = sys.argv[3]
+
+input_dir = os.path.dirname(description_yml)
+property_file = os.path.relpath(property_prp, input_dir)
+input_file = os.path.relpath(benchmark_smt2, input_dir)
 
 property_check_sat = {
-    "property_file": "../properties/check-sat.prp",
+    "property_file": property_file,
 }
 
-cmd = sys.argv[3:]
+properties = [property_check_sat]
+
+options = {"language": "SMT-LIB"}
+
+data = {
+    "format_version": "2.0",
+    "input_files": input_file,
+    "properties": properties,
+    "options": options,
+}
+
+cmd = sys.argv[4:]
 
 result = subprocess.run(cmd + [benchmark_smt2], capture_output=True)
 output = str(result.stdout, "utf-8")
@@ -27,22 +43,6 @@ match status:
     case _:
         # property_check_sat["expected_verdict"] = "unknown"
         pass
-
-input_dir = os.path.dirname(description_yml)
-input_file = os.path.relpath(benchmark_smt2, input_dir)
-
-properties = [property_check_sat]
-
-options = {"language": "SMT-LIB"}
-
-data = {
-    "format_version": "2.0",
-    "input_files": input_file,
-    "properties": properties,
-    "options": options,
-}
-
-# yaml.dump(data, sys.stdout)
 
 with open(description_yml, "w") as file:
     yaml.dump(data, file)
