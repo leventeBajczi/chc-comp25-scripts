4254e8f6273abeaf7ac253d94bf235feeea0d169
Gidon Ernst <gidon.ernst@sosy.ifi.lmu.de>
2025-05-12 16:51:12 +0200

    update plots

--- Makefile
+++ Makefile
@@ -1,14 +1,18 @@
 .PHONY: help yml
 
-SMT2 ?= $(shell find ../chc-comp24-benchmarks -iname "*.smt2")
+PRP ?= ../chc-comp25-benchmarks/properties/check-sat.prp
+SMT2 ?= $(shell find ../chc-comp25-benchmarks/ -iname "*.smt2")
 YML = $(SMT2:.smt2=.yml)
 
-SOLVER ?= z3 -t:500
+SOLVER ?= z3 -t:100
 
 help:
 	@echo "Please refer to README.md"
 
+smt2:
+	echo $(SMT2)
+
 yml: $(YML)
 
 %.yml: %.smt2
-	./create-benchexec-yml.py $< $@ $(SOLVER)
+	./create-benchexec-yml.py $(PRP) $< $@ $(SOLVER)
--- plot.py
+++ plot.py
@@ -13,9 +13,11 @@ import pandas as pd
 category = sys.argv[1]
 files = sys.argv[2:]
 
+URL="https://www.cip.ifi.lmu.de/~ernstg/chc-comp2025/"
+
 config = {
     "CHC2C 1.0": {"label": "CHC2C", "color": "black", "marker": "s"},
-    "ChocoCatalia": {"label": "Choco Catalia", "color": "chocolate", "marker": "s"},
+    "ChocoCatalia ": {"label": "Choco Catalia", "color": "chocolate", "marker": "s"},
     "Eldarica v2.2": {"label": "Eldarica", "color": "red", "marker": "o"},
     "Golem 0.7.1": {"label": "Golem", "color": "gray", "marker": "o"},
     "MuCyc NO_VERSION_UTIL": {"label": "MuCyc", "color": "deepskyblue", "marker": "s"},
@@ -27,12 +29,12 @@ config = {
         "marker": "o",
     },
     "Ultimate TreeAutomizer (unknown version)": {
-        "label": "U TreeAutomizer",
+        "label": "Ultimate TreeAutomizer",
         "color": "orange",
         "marker": "v",
     },
     "Ultimate Unihorn (unknown version)": {
-        "label": "U Unihorn",
+        "label": "Ultimate Unihorn",
         "color": "gold",
         "marker": "^",
     },
@@ -42,19 +44,23 @@ plt.xlabel("Cumulative Score (solved tasks)")
 plt.ylabel("CPU time (s)")
 plt.title("CHC-COMP 2025 - " + category)
 
+results = []
+
 for path in files:
     with open(path, newline="") as file:
-        print(path)
         reader = csv.reader(file, delimiter="\t")
         tools = next(reader)
         tool = tools[2]
         style = config[tool]
 
-        category = next(reader)
+        style["marker"] = "."
+
+        _ = next(reader)
 
         header = next(reader)
         assert header[2] == "status"
         assert header[3] == "cputime (s)"
+        assert header[4] == "walltime (s)"
 
         data = [row for row in reader if row[3]]
         data.sort(key=lambda row: float(row[3]))
@@ -63,19 +69,84 @@ for path in files:
         ydata = []
         score = 0
 
-        for _, expected, verdict, cputime, *rest in data:
+        correct_true = 0
+        correct_false = 0
+        unknown_true = 0
+        unknown_false = 0
+        wrong_true = 0
+        wrong_false = 0
+        
+        total_cputime = 0.0
+        total_walltime = 0.0
+
+        for _, expected, verdict, cputime, walltime, *rest in data:
             if expected == verdict:
-                score = score + 1
+                score += 1
                 xdata.append(score)
                 ydata.append(float(cputime))
 
+            total_cputime += float(cputime)
+            total_walltime += float(walltime)
+
+            match (expected, verdict):
+                case ("true", "true"):
+                    correct_true += 1
+                case ("false", "false"):
+                    correct_false += 1
+                case ("false", "true"):
+                    wrong_true += 1
+                case ("true", "false"):
+                    wrong_false += 1
+                case (_, "true"):
+                    unknown_true += 1
+                case (_, "false"):
+                    unknown_false += 1
+
         plt.plot(xdata, ydata, **style)
 
+        results.append((style["label"], path, correct_true, correct_false, unknown_true, unknown_false, total_cputime, total_walltime))
 
 plt.grid(True)
 plt.yscale('log')
 plt.yticks((1,10,100,1000))
-plt.ylim((-200, 2000))
 
 plt.legend()
-plt.show()
+plt.savefig(category + ".svg")
+
+results.sort(key=lambda row: -row[2] - row[3])
+
+print("""<h4><a href="%s">%s (⇒ compare results)</a></h4>
+
+<div class="results">
+        <div style="float: left">
+            <img src="2025/%s.svg" height="240px" onclick="window.open(this.src)"/>
+        </div>
+<table>
+    <tr>
+        <th class="left">Tool</th>
+        <th>sat</th>
+        <th>unsat</th>
+        <th>CPU (s)</th>
+        <th>wall (s)</th>
+    </tr>
+""" % (URL + "test/results/" + category + ".results.table.html", category, category))
+
+for tool, path, correct_true, correct_false, unknown_true, unknown_false, total_cputime, total_walltime in results:
+    link = path[:-4] + ".html"
+    print("<tr class=\"entry\">")
+    print("    <td><a href=\"" + URL + link + "\">" + tool + "</a></td>")
+    print("    <td>%d</td>" % correct_true)
+    print("    <td>%d</td>" % correct_false)
+    # print("    <td>%d</td>" % unknown_true)
+    # print("    <td>%d</td>" % unknown_false)
+    # print("    <td>%d</td>" % wrong_true)
+    # print("    <td>%d</td>" % wrong_false)
+    print("    <td>%.0f</td>" % total_cputime)
+    print("    <td>%.0f</td>" % total_walltime)
+    print("</tr>")
+    print()
+
+print("""</table>
+    <div style="clear: both"></div>
+</div>
+""")
